8bff1eb6d95b3b6e4eb4115b97e693850f8c7382
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-24 16:16:45 +0200

    ML2 - Fatta

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 let listaSpesa = ["Acqua", "Monster Energy", "Vegan Burger"];
 
+/*
 //Ciclo con for
 for (let i = 0; i < listaSpesa.length; i++) {
     const element = listaSpesa[i];
     console.log(element);
 }
-
+*/
+let i=0;
 //Ciclo con while
-while
+while (i < listaSpesa.length) {
+    const element = listaSpesa[i];
+    console.log(element);
+
+    i++;
+}
+i=0; //Reset in caso venga riutilizzata in futuro
