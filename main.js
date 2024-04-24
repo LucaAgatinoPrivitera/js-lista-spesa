3c63bc43dda999e29ae463ef16ed3466582e3014
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-24 16:38:59 +0200

    Ml3 - Sto cercando di capire come far generare un <li> e mettere le cose dentro

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 let listaSpesa = ["Acqua", "Monster Energy", "Vegan Burger"];
-
 /*
 //Ciclo con for
 for (let i = 0; i < listaSpesa.length; i++) {
@@ -7,12 +6,18 @@ for (let i = 0; i < listaSpesa.length; i++) {
     console.log(element);
 }
 */
+
+let text = `Hello World!`;
+const newLi = document.createElement("li");
+
 let i=0;
 //Ciclo con while
 while (i < listaSpesa.length) {
     const element = listaSpesa[i];
     console.log(element);
 
+    document.querySelector(".lista").innerHTML += `${element}`
+    document.getElementsByClassName("lista").innerHTML = ("Sei iscritto");
     i++;
 }
 i=0; //Reset in caso venga riutilizzata in futuro
